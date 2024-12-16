403b60ccc5833f67d11a85fccccc4bc80f67ade0
skh9301 <skh9301@naver.com>
2024-12-16 12:21:41 +0900

    #1 notice 검색 기능 만들기

--- src/components/page/Board/Notice/NoticeSearch.vue
+++ src/components/page/Board/Notice/NoticeSearch.vue
@@ -1,54 +1,79 @@
 <template>
-    <div class="search-box">
-        <input />
-        <input type="date" />
-        <input type="date" />
-        <button>검색</button>
-        <button>신규등록</button>
-    </div>
+  <div class="search-box">
+    <input v-model.lazy="keyword" />
+    <input type="date" v-model="searchStartDate" />
+    <input type="date" v-model="searchEndDate" />
+    <button @click="handlerSearch">검색</button>
+    <button>신규등록</button>
+  </div>
 </template>
-<script></script>
+<script setup>
+import router from "@/router";
+const keyword = ref(""); // ref - 값이 변경되면 화면이 재렌더링
+const searchStartDate = ref("");
+const searchEndDate = ref("");
+
+//keyword 값이 변경 될 때마다 (state변경) 이하의 함수가 실행된다.
+// watch(keyword, () => {
+//   //keyword 자리에는 반응형객체(ref) 포함된 값이들어가야함
+//   console.log(keyword.value); //키워드 값을 직접적으로 관리하고싶을 땐 .value
+// });
+
+const handlerSearch = () => {
+  const query = [];
+  !keyword.value || query.push(`searchTitle=${keyword.value}`);
+  !searchStartDate.value || query.push(`searchStartDate=${searchStartDate.value}`);
+  !searchEndDate.value || query.push(`searchEndDate=${searchEndDate.value}`);
+  const queryString = query.length > 0 ? `?${query.join("&")}` : ""; //url 뒤에 붙이기위한 string
+
+  router.push(queryString);
+};
+
+// 인자로 받는 함수안에 반응형 객체 (ref 같은거)가 있으면, 객체가 변경 도리 때 마다, 해당 함수를 실행 시켜줌
+// 근데 ,  밑에 watchEffect는 ref같은거 없어요. 그래서 그냥 새로고침 누르면 최초에 한번 실행되는 것입니다.
+watchEffect(() => window.location.search && router.push(window.location.posthname, { replace: ture }));
+</script>
 
 <style lang="scss" scoped>
 .search-box {
-    margin-bottom: 10px;
-    display: block;
-    float: inline-end;
+  margin-bottom: 10px;
+  display: block;
+  float: inline-end;
 }
 
 input {
-    padding: 8px;
-    margin-top: 5px;
-    margin-bottom: 5px;
-    margin-right: 5px;
-    border-radius: 4px;
-    border: 1px solid #ccc;
+  padding: 8px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  margin-right: 5px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
 }
 
 button {
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    border: none;
-    color: white;
-    width: 70px;
-    padding-top: 8px;
-    padding-bottom: 8px;
-    font-size: 12px;
-    margin: 4px 2px;
-    cursor: pointer;
-    border-radius: 12px;
-    box-shadow: 0 4px #999;
-    background-color: #3bb2ea;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border: none;
+  color: white;
+  width: 70px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  font-size: 12px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 12px;
+  box-shadow: 0 4px #999;
+  background-color: #3bb2ea;
 
-    &:hover {
-        background-color: #45a049;
-    }
+  &:hover {
+    background-color: #45a049;
+  }
 
-    &:active {
-        background-color: #3e8e41;
-        box-shadow: 0 2px #666;
-        transform: translateY(2px);
-    }
+  &:active {
+    background-color: #3e8e41;
+    box-shadow: 0 2px #666;
+    transform: translateY(2px);
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/Login/Login.vue';
-import DashBoard from '../views/DashBoard/DashBoard.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Login/Login.vue";
+import DashBoard from "../views/DashBoard/DashBoard.vue";
+import Notice from "../views/Board/Notice.vue";
 
-const routes = [
-    {
-        path: '/',
-        name: 'login',
+const routes = [{
+        path: "/",
+        name: "login",
         component: Login,
     },
     {
-        path: '/vue',
-        name: 'vue',
+        path: "/vue",
+        name: "vue",
         component: DashBoard,
+        children: [{
+            path: "board",
+            name: "board",
+            children: [{
+                // notice.do => localhost:5173/board/notice.do
+                // /notice.do => localhost:5173/notice.do
+                path: "notice.do",
+                name: "notice",
+                component: Notice,
+            }, ],
+        }, ],
     },
 ];
 
 const router = createRouter({
-    history: createWebHistory('/'),
+    history: createWebHistory("/"),
     routes,
 });
 
